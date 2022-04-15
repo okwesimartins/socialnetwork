7550bb5335a6ded616ede54de18acc297e05f2a0
pikcars <44181174+pikcars@users.noreply.github.com>
2022-04-15 19:07:40 +0100

    new

--- app/routers/user.py
+++ app/routers/user.py
@@ -37,5 +37,5 @@ def create_user(user: schema.Users, db: Session = Depends(get_db)):
 def getUser(id: int, db: Session = Depends(get_db)):
     user=db.query(models.User).filter(models.User.id == id).first()
     if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"User with id: {id} does not exist")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"User with id: {id} does not exist here")
     return user
